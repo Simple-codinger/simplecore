b05cdfc3cb8f7cb06ab472176619dcee9d4f0656
Simple-codinger <vincentgierisch@gmail.com>
2015-09-30 19:46:12 +0200

    Echo
    Echo in Core hinzugefügt

--- core/main.php
+++ core/main.php
@@ -1,6 +1,6 @@
 <?php
 /*Author Gierisch Vincent, PersNr.: 4974, Tel.: 2350
-Thanks to Domink Ganic for supporting me*/		
+Thanks to Domink Ganic for supporting me*/
 	namespace trainMan\core;
 
 	class coreMain{
@@ -9,7 +9,7 @@ Thanks to Domink Ganic for supporting me*/
 			spl_autoload_register(array($this, 'core_loader'));
 			$this->_defineClasses();
 		}
-		
+
 		private function _defineClasses(){
 			global $cSession;
 			$cSession = new \trainMan\core\session();
@@ -27,7 +27,7 @@ Thanks to Domink Ganic for supporting me*/
        		$file = ltrim($className, '\\');  // Evntl. vorangegangenen Backslashes entfernen
 	        $file_array = explode('\\', $file);
 	        array_shift($file_array); // Root-Namespace entfernen
-	 
+
 	        $file = implode(DIRECTORY_SEPARATOR, $file_array);
 	        $path = dirname(__DIR__) . DIRECTORY_SEPARATOR . $file . '.php';
 	        if(file_exists($path)) include $path; // Pfad der Klasse einbinden
@@ -37,7 +37,7 @@ Thanks to Domink Ganic for supporting me*/
 		public function getUser(){
 			return unserialize($_SESSION[\trainMan\core\config::getConfiguration("SessionNameUser")]);
 		}
-		
+
 		public function mSubString($mKey, $mFrom, $mTo) {
 			$mMaxLength = $mTo - $mFrom;
 			if(strlen($mKey) > $mMaxLength) {
@@ -49,4 +49,5 @@ Thanks to Domink Ganic for supporting me*/
 	}
 	global $cMain;
 	$cMain = new coreMain();
+	echo "TEST";
 ?>
