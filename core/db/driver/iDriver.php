4fbe6de20afbd9042e26be613075d3960f766ee6
Simple-codinger <vincentgierisch@gmail.com>
2016-05-07 16:05:08 +0200

    deleted db classes: wrong structure

--- core/db/driver/iDriver.php
+++
@@ -1,7 +0,0 @@
-<?php
-namespace simplecore\core\db\driver;
-	interface iDriver
-	{
-		public function connect(array $params, $username = null, $password = null, array $driverOptions = array());
-	}
-?>
